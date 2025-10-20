064ff9003e56fe503b0dbaaddecf9ac8c0cd1d6e
Waleed5423 <waleed54234@gmail.com>
2025-10-20 20:34:12 +0500

    d

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -3,7 +3,7 @@ import { SettingsContent } from "@/components/settings-content";
 
 export default function SettingsPage() {
   return (
-    <ProtectedRoute allowedUserTypes={["admin"]} allowedAdminTypes={["super"]}>
+    <ProtectedRoute allowedUserTypes={["admin"]}>
       <SettingsContent />
     </ProtectedRoute>
   );
